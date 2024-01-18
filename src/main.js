7d554394db493b47793cfa0dea71996a223d3494
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2024-01-18 13:51:33 -0300

    adicionando o bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './style.css'
 import App from './App.vue'
 
